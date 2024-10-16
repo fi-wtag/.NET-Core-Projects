68c59f8b89f78d54a7bec995df31e530218bdaa7
Faridul Islam <faridul.islam@welldev.io>
2024-10-16 16:40:16 +0600

    Added Gitignore and added Company Employees Project from the book

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+
+        public WeatherForecastController(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            //_repository.Company.AnyMethodFromCompanyRepository();
+            //_repository.Employee.AnyMethodFromCompanyRepository();
+
+            return new string[] { "Value1, Valu2" };
+        }
+    }
+}
